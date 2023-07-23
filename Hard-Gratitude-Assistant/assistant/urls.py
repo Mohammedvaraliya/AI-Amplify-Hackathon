7f2650143325ceca82aead8547192679178183b0
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2023-07-23 15:34:07 +0530

    Update Easy solution

--- Hard-Gratitude-Assistant/assistant/urls.py
+++ Hard-Gratitude-Assistant/assistant/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('query/', views.query, name='query'),
     path('assistant/', views.assistant, name='assistant')
 ]
